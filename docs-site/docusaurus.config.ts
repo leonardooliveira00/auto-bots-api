c52231ea5967ae60c50404175605c3279b039a91
Leonardo <leodev00@outlook.com>
2026-06-07 16:56:02 -0300

    fix: restore index.tsx and update landing page link to overview

--- docs-site/docusaurus.config.ts
+++ docs-site/docusaurus.config.ts
@@ -29,7 +29,7 @@ const config: Config = {
   organizationName: 'leooliveira00', // Usually your GitHub org/user name.
   projectName: 'auto-bots-api', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
